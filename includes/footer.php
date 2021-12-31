e14a7698e2223bf421751be13b91d3aaea041ba0
sane8s <saneyates@gmail.com>
2021-12-31 11:46:35 -0600

    Replace files
    Updated to Bootstrap 5

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,12 @@
-<div id="footer">
-	<!-- Support iWebKit by sending us traffic; please keep this footer on your page, consider it a thank you for my work :-) -->
-    <!--<a class="noeffect" href="http://snippetspace.com">iPhone site powered by iWebKit</a></div>-->
-    <a class="noeffect" href="/vaudeville/about.php">About Random Vaudeville</a>
+<!-- Site footer -->
+<div class="container">
+  <footer class="py-3 my-4">
+    <ul class="nav justify-content-center pb-3 mb-3">
+      <li class="nav-item"><a href="/vaudeville/about.php" class="nav-link px-2 text-muted">About Random Vaudeville</a></li>
+    </ul>
+    <p class="text-center text-muted">&copy; 2021</p>    
+  </footer>
+</div>
+</main>    
 </body>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -1,27 +1,17 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<meta content="yes" name="apple-mobile-web-app-capable" />
-<meta content="index,follow" name="robots" />
-<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
-<link href="pics/homescreen.gif" rel="apple-touch-icon" />
-<meta content="minimum-scale=1.0, width=device-width, maximum-scale=0.6667, user-scalable=no" name="viewport" />
-<link href="css/style.css" rel="stylesheet" media="screen" type="text/css" />
-<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-<script src="javascript/functions.js" type="text/javascript"></script>
-<title>Random Vaudeville</title>
-<meta content="iPod,iPhone,Webkit,iWebkit,Website,Create,mobile,Tutorial,free" name="Keywords" />
-<meta content="A little iWebKit history lesson" name="description" />
+  <title>Random Vaudeville</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
-<body>
-<div id="topbar">
-	<!--<div id="leftnav">
-		<a href="index.html"><img alt="home" src="images/home.png" /></a></div>-->
-	<div id="title">
-        Random Vaudeville
+<body class="bg-dark">
+  <main>
+    <!-- Site header -->
+  <header>
+    <div class="container-fluid p-1 bg-secondary text-black text-center">
+        <h1><a href="/vaudeville" class="nav-link px-2 text-black">Random Vaudeville</a></h1>
     </div>
-</div>
-<div id="duobutton">
-	<div class="links">
-		<a href="/vaudeville/">Vaudeville Me!</a></div>
-</div>
+</header>
