15392cb5e166af7477512aaa8ab712519abb287d
sane8s <saneyates@gmail.com>
2022-01-14 09:40:53 -0600

    Add files via upload
    Added include for analytics script

--- includes/header.php
+++ includes/header.php
@@ -7,6 +7,7 @@
   <meta name="theme-color" content="#000000">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+  <?php include "ga.html";?>
 </head>
 <body class="bg-dark">
   <main>
